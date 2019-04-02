1eee5f27cedd6e6b4faf03de8eb96bad61bd7bd6
Michael Kelly <mike.s.kelly@icloud.com>
2019-04-02 11:31:56 -0700

    Hit a wall, committing while trying to work it out

---
+++ DontWineAboutIt/DontWineAboutIt/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DontWineAboutIt.Models;
+
+namespace DontWineAboutIt.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string name, int maxPrice, int minPoints)
+        {
+            Wine wineSearch = new Wine();
+            string Name = name;
+            int MaxPrice = maxPrice;
+            int MinPoints = minPoints;
+            return wineSearch;
+        }
+
+        [HttpPost]
+        public IActionResult Result()
+        {
+            return RedirectToAction("Results", wineList);
+        }
+    }
+}
---
+++ DontWineAboutIt/DontWineAboutIt/Models/Wine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DontWineAboutIt.Models
+{
+    public class Wine
+    {
+        public string Number { get; set; }
+        public string Country { get; set; }
+        public string Description { get; set; }
+        public string Designation { get; set; }
+        public string Points { get; set; }
+        public string Price { get; set; }
+        public string Province { get; set; }
+        public string Region_1 { get; set; }
+        public string Region_2 { get; set; }
+        public string Variety { get; set; }
+        public string Winery { get; set; }
+
+        public static string[] GetWineList()
+        {
+            string path = "~/wine.csv";
+            string[] wineList = File.ReadAllLines(path);
+
+            return wineList;
+        }
+    }
+}
